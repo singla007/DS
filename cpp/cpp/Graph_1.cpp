59792096898ac6d55415e3ba37de8989b1330538
yogesh kumar singla <eryogeshkumarsingla007@gmail.com>
2020-10-31 09:36:20 +0530

    adding graph

---
+++ cpp/cpp/Graph_1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void addedge(vector<int> g[],int a,int b) {
+
+}
+
+int main() {
+	const int V = 6;
+	vector<int> g[V];
+	return 0;
+}
--- cpp/cpp/pointer_1.cpp
+++ cpp/cpp/pointer_1.cpp
@@ -4,17 +4,51 @@ using namespace std;
 struct Node {
 	int value;
 	Node* next;
-	Node(int v) {
+	Node(int v=-1) {
 		value = v;
 		next = NULL;
 	}
 };
-int main() {
-	Node* head = new Node(5);
-	Node* temp1 = new Node(3);
-	Node* temp2 = new Node(4);
-	head->next = temp1;
-	temp1->next = temp2;
+void push(Node* head, int x) {
+	Node* temp = new Node(x);
 
+	while (head->next != NULL) {
+		head = head->next;
+	}
+	head->next = temp;
+}
+void print_list(Node* head) {
+	if (head == NULL) {
+		cout << "Empty";
+	}
+	while (head != NULL) {
+		cout << head->value<<" ";
+		head = head->next;
+	}
+}
+void insert_after(Node* head, int x, int y) {
+	Node* temp = new Node();
+	if (head == NULL) {
+		cout << "Empty";
+	}
+	while (head->next != NULL) {
+		if (head->value == x) {
+			temp = head->next;
+			break;
+		}
+		head = head->next;
+	}
+	head->next = new Node(y);
+	head->next->next = temp;
+}
+int main() {
+	Node* head = new Node(3);
+	push(head, 5);
+	push(head, 3);
+	push(head, 7);
+	push(head, 1);
+	push(head, 4);
+	insert_after(head, 3, 10);
+	print_list(head);
 	return 0;
 }
